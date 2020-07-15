310ab25eaf10fb4a5c8abaff561c298cda782c01
Gustavo Silva <gustavo24gss@gmail.com>
2020-07-14 21:38:14 -0300

    fix

--- auctions/urls.py
+++ auctions/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("new", views.new, name="new"),
     path("listing/<int:id>", views.listing, name="listing"),
     path("watchlist", views.watchlist, name="watchlist"),
-    path("categor/<int:id>", views.category, name="category")
+    path("category/<int:id>", views.category, name="category")
 ]
